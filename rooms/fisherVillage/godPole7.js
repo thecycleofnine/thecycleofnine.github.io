b7036891510a0df21ed3742cc3e8de27547248f1
e2f5db0 <sami.toiskallio@protonmail.com>
2022-08-15 14:25:40 +0300

    Add content to godPole7

--- rooms/fisherVillage/godPole7.js
+++ rooms/fisherVillage/godPole7.js
@@ -2,20 +2,36 @@ const godPole7 = {
     id: 'godPole7',
     area: 'Fisher village',
     name: `Seventh wooden pole`,
-    desc: `TBA`,
+    desc: `The carving depicts a fertility god of godly dimensions. There's a ***pole*** within the pole and everything there needs to be.
+    The god is holding an ***antler*** and is accompanied by a ***boar***.`,
     printDescriptions: true,
     onFeel: () => {
         if (player.eyesAreOpen) {
-            println(`TBAopen`)
+            println(`You feel the fair weather caressing you.`)
         } else {
-            println(`TBAclosed`)
+            println(`You feel blessed rains approaching. The land is flourishing.`)
         }
     },
     items: [
         {
             name: 'pole',
-            onSwing: () => println(`TBA`),
-            onEat: () => println(`TBA`),
+            desc: `It is majestetic. It is godly. It is everything it needs to be.`,
+            onSwing: () => println(`The mighty ***pole*** parries the swing playfully.`),
+            onEat: () => println(`By the master of Boar, the Golden mane,
+                                may thou be blessed with fertile rain.`)
+        },
+        {
+            name: 'antler',
+            desc: `It seems to be a replacement for a sword once bargained away.`,
+            onSwing: () => println(`It can take a hit!`),
+            onEat: () => println(`You try to munch on the ***antler***
+            but it's too sharp!`),
+        },
+        {
+            name: 'boar',
+            desc: `The boar has golden bristles in its mane. They glow in the dark.`,
+            onSwing: () => println(`The ***antler*** shifts to block the blow.`),
+            onEat: () => println(`You try to take a bite out of the ***boar*** but the ***antler*** shifts to protect it.`),
         }
     ],
     exits: [
